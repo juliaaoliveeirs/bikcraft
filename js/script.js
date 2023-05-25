833db2ee5513d38878ed431d392be30f086d0884
Julia Silva <julia.oliveira.silva@outlook.com>
2023-05-25 11:22:39 -0300

    animação perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -24,3 +24,22 @@ function ativarProduto(paramentro) {
 }
 
 paramentros.forEach(ativarProduto);
+
+// Perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const constrols = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(constrols);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
